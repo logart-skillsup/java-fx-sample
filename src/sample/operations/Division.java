3c3c2ba0066a9914f79f118988c4766c24658a7d
Nordgaimer <nordgaimer7@gmail.com>
2014-07-01 21:21:00 +0300

    Added to Substract method bug fix

--- src/sample/operations/Division.java
+++ src/sample/operations/Division.java
@@ -5,6 +5,10 @@ import sample.BinaryOperation;
 public class Division implements BinaryOperation {
     @Override
     public double execute(double first, double second) {
+        //Avoiding ArithmeticException
+        if (second==0){
+            return 0;
+        }
         return first/second;
     }
 }
